3572148ec898948d11d18fa8cda7a242ef6dbbcd
kumakovkarim-lab <kumakovkarim@gmail.com>
2026-01-21 13:11:41 +0500

    Update DatabaseConfig.java

--- src/main/java/com/warehouse/DatabaseConfig.java
+++ src/main/java/com/warehouse/DatabaseConfig.java
@@ -1,8 +1,6 @@
 package com.warehouse;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 public class DatabaseConfig {
     // параметры жля коннекта с PostgreSQL
     // URL, login, password и + метод getConnection()
