0704b17d28bd9ddf61e366301c18f472766865b8
beybarys0905-eng <beybarys0905@gmail.com>
2026-01-21 13:19:27 +0500

    Commit

--- src/main/java/com/warehouse/DatabaseConfig.java
+++ src/main/java/com/warehouse/DatabaseConfig.java
@@ -1,6 +1,8 @@
 package com.warehouse;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DatabaseConfig {
     // параметры жля коннекта с PostgreSQL
     // URL, login, password и + метод getConnection()
