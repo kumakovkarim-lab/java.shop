e15d56aece84511c9588ef1bc8598e188743d6c6
siakinow <aybarerkebulan07@gmail.com>
2026-01-23 00:22:22 +0500

    Update Product.java

--- src/main/java/com/warehouse/model/Product.java
+++ src/main/java/com/warehouse/model/Product.java
@@ -1,6 +1,26 @@
 package com.warehouse.model;
 
 public class Product {
-    // доабавьте поля id, name, price и колво товаров + геттеры и сеттеры добавитее
+    private int id;
+    private String name;
+    private String category;
+    private BigDecimal price;
+    private int quantity;
 
+    public Product()
+}
+
+public Product(String name, String category, BigDecimal price, int quantity) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+public Product(int id, String name, String category, BigDecimal price, int quantity) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.price = price;
+    this quantity = quantity;
 }
