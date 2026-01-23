ec29e20d90ad05429201eadd515ee9965593c4ed
siakinow <aybarerkebulan07@gmail.com>
2026-01-23 19:27:21 +0500

    Update Product.java

--- src/main/java/com/warehouse/model/Product.java
+++ src/main/java/com/warehouse/model/Product.java
@@ -24,3 +24,44 @@ public Product(int id, String name, String category, BigDecimal price, int quant
     this.price = price;
     this quantity = quantity;
 }
+
+public int getld(){
+    return id;
+}
+
+public void setld(int id){
+    this.id = id;
+}
+
+public String gerName(){
+    return name;
+}
+
+public void setName(String name){
+    this.name = name;
+}
+    
+public String getCategory(){
+    return category;
+}
+
+public void setCategory(String category){
+    this.category = category;
+}
+
+public BigDecimal getPrice(){
+    return price;
+}
+
+public void setPrice(BigDecimal price){
+    this.price = price;
+}
+
+public int getQuantity(){
+    return quantity;
+}
+
+public void setQuantity(int quantity){
+    this.quantity = quantity;
+}
+    
