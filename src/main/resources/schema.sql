7877e0e06291321fc983e8151c39c83a7e19f080
Kumakov Karim <kumakovkarim@gmail.com>
2026-01-31 08:19:29 +0500

    m

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,32 +1,7 @@
-DROP TABLE IF EXISTS order_items CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS products CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
-
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    role VARCHAR(20) NOT NULL
-);
-
-CREATE TABLE products(
+CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price NUMERIC(10,2) NOT NULL,
-    quantity INT NOT NULL,
-    category_id INT REFERENCES categories(id) ON DELETE
-SET NULL
-);
-
-CREATE TABLE orders (
-    id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(id),
-    order_date  TIMESTAMP
-);
-
-INSERT INTO categories (name) VALUES ('Electronics'), ('Food'),
-('Books');
-INSERT INTO users(username,role) VALUES ('admin_user',
-'ADMIN'), ('customer_zero', 'CUSTOMER');
-
+    category VARCHAR(255) NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    quantity INT NOT NULL DEFAULT 0
+    );
--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -4,4 +4,4 @@ CREATE TABLE IF NOT EXISTS products (
     category VARCHAR(255) NOT NULL,
     price NUMERIC(10, 2) NOT NULL,
     quantity INT NOT NULL DEFAULT 0
-);
+    );
